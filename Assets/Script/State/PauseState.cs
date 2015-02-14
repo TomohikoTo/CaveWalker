a2a883d70f87cc8e1ffbd389096f269488063b24
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-02-14 20:41:28 +0900

    各種Stateスクリプト修正

--- Assets/Script/State/PauseState.cs
+++ Assets/Script/State/PauseState.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PauseState : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+namespace DreamWalker {
+	public class PauseState : IState {
+		private GameStateManager manager;
+		
+		public PauseState(GameStateManager GSManager) {
+			//初期化
+			manager = GSManager;
+			Time.timeScale = 0;
+		}
+		
+		public void StateUpdate() {
+			
+			//更新処理
+			if(Input.anyKey) { // 何らかのキーを押すとMenuStateに遷移
+				
+				
+				//	manager.SwitchState(new MenuState(manager));
+				//Application.LoadLevel("Menu");
+			}
+		}
+		
+		public void Render() {
+			//描画等
+			
+		}
+		
 	}
 }
--- Assets/Script/State/ResultState.cs
+++ Assets/Script/State/ResultState.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ResultState : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+namespace DreamWalker {
+	public class ResultState : IState {
+		private GameStateManager manager;
+		
+		public ResultState(GameStateManager GSManager) {
+			//初期化
+			manager = GSManager;
+			Time.timeScale = 0;
+		}
+		
+		public void StateUpdate() {
+			
+			//更新処理
+			if(Input.anyKey) { // 何らかのキーを押すとMenuStateに遷移
+				
+				
+				//	manager.SwitchState(new MenuState(manager));
+				//Application.LoadLevel("Menu");
+			}
+		}
+		
+		public void Render() {
+			//描画等
+			
+		}
+		
 	}
 }
