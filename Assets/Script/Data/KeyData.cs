1aea0f285bb70eb56962bae46dd550cb4ccd5704
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-02-26 23:35:56 +0900

    keydataスクリプト修正

--- Assets/Script/Data/KeyData.cs
+++ Assets/Script/Data/KeyData.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class KeyData : MonoBehaviour {
 
+
+	public void GetKeyPoint(){
+
+	}
+
+	public void SetKeyPoint(){
+	}
+
 	// Use this for initialization
 	void Start () {
 	
