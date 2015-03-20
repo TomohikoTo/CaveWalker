929328048981ee6ba598b2825dae571722ae1cef
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-03-20 23:59:19 +0900

    キーコンフィグ修正

--- Assets/Script/Mover/KeyConfig.cs
+++ Assets/Script/Mover/KeyConfig.cs
@@ -1,36 +1,36 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace DreamWalker {
 public class KeyConfig : MonoBehaviour {
-
-		Event keySet = Event.current;
+ 
+		public KeyCode[] codes;
 		// キーコンフィグの設定情報
-		public static Dictionary<string, KeyCode> Config
-			= new Dictionary<string, KeyCode>();
+		public static Dictionary<int, KeyCode> Config
+			= new Dictionary<int, KeyCode>();
 
 
 		// キー状態取得メソッド
-		public static bool GetKey(string key) {
+		public static bool GetKey(int key) {
 			return Input.GetKey(Config[key]);
 		}
-		public static bool GetKeyDown(string key) {
+		public static bool GetKeyDown(int key) {
 			return Input.GetKeyDown(Config[key]);
 		}
-		public static bool GetKeyUp(string key) {
+		public static bool GetKeyUp(int key) {
 			return Input.GetKeyUp(Config[key]);
 		}
 		// キーコンフィグ設定
 		void Start() {
-
-			KeyConfig.Config["1"] = KeyCode.A;
-			KeyConfig.Config["2"] = KeyCode.W;
-			KeyConfig.Config["3"] = KeyCode.S;
-			KeyConfig.Config["4"] = KeyCode.E;
-			KeyConfig.Config["5"] = KeyCode.D;
-			KeyConfig.Config["6"] = KeyCode.R;
-			KeyConfig.Config["7"] = KeyCode.F;
+			myNumbers[0] = KeyCode.A;
+			KeyConfig.Config[0] = KeyCode.A;
+			KeyConfig.Config[1] = KeyCode.W;
+			KeyConfig.Config[2] = KeyCode.S;
+			KeyConfig.Config[3] = KeyCode.E;
+			KeyConfig.Config[4] = KeyCode.D;
+			KeyConfig.Config[5] = KeyCode.R;
+			KeyConfig.Config[6] = KeyCode.F;
 		}
 		
 		// キーコンフィグ使用例
@@ -62,15 +62,15 @@ public class KeyConfig : MonoBehaviour {
 		}
 
 		void CheckKey(){
-
+			int i ;
 			//キー入力取得用イベント
-			foreach(KeyCode kName in KeyConfig.Config)  
-				//キーが重複してないか確認
-				if( (Input.GetKeyDown(KeyConfig.Config["1"]))){
-
+			for( i = 0 ; i < Config.Count ; i++){
+				if( Input.GetKeyDown(KeyConfig.Config[i]) ){
 					Debug.Log("入力キーが重複してます " );
-				
 				}
+
+			}
+
 				
 			
 		}
